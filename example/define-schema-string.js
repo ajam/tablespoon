90be35f8f5d5de64dc82c07506ae0a1191b3c15a
mhkeller <michael.keller@gmail.com>
2014-01-10 16:58:49 -0500

    proper indents

--- example/define-schema-string.js
+++ example/define-schema-string.js
@@ -36,19 +36,19 @@ sequel.createTableSync('cities', data, schema_string)
 // Get the rows that don't have 15
 bk.query('SELECT * FROM cities WHERE 15 != ALL (temp)', function(rows){
 	console.log(rows)
-/*[
-  { uid: '1', city: 'New York',     temp: [ 0, 35 ], country: 'USA' },
-  { uid: '3', city: 'Paris',        temp: [ 2, 33 ], country: 'France' },
-  { uid: '4', city: 'Marseille',    temp: [ 5, 27 ], country: 'France' },
-  { uid: '5', city: 'London',       temp: [ 2, 25 ], country: 'UK' } ]*/
+	/*[
+	  { uid: '1', city: 'New York',     temp: [ 0, 35 ], country: 'USA' },
+	  { uid: '3', city: 'Paris',        temp: [ 2, 33 ], country: 'France' },
+	  { uid: '4', city: 'Marseille',    temp: [ 5, 27 ], country: 'France' },
+	  { uid: '5', city: 'London',       temp: [ 2, 25 ], country: 'UK' } ]*/
 })
 
 // Get the one that does
 bk.query.each('SELECT * FROM cities WHERE 15 = ANY (temp)', function(row, query){
 	console.log(query)
 	console.log(row)
-/*
-SELECT * FROM cities WHERE 15 = ANY (temp)
-{ uid: '2',  city: 'Los Angeles',  temp: [ 15, 35 ],  country: 'USA' }*/
+	/*
+	SELECT * FROM cities WHERE 15 = ANY (temp)
+	{ uid: '2',  city: 'Los Angeles',  temp: [ 15, 35 ],  country: 'USA' }*/
 
 })
