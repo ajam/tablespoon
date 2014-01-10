b6a8923bc836f6a92181a565d96f5d66412cd295
mhkeller <michael.keller@gmail.com>
2014-01-10 16:55:40 -0500

    example output

--- example/define-schema-string.js
+++ example/define-schema-string.js
@@ -34,11 +34,24 @@ var schema_string = 'city TEXT, temp integer[], country TEXT';
 sequel.createTableSync('cities', data, schema_string)
 
 // Get the rows that don't have 15
-sequel.query('SELECT * FROM cities WHERE 15 != ALL (temp)', function(rows){
+bk.query('SELECT * FROM cities WHERE 15 != ALL (temp)', function(rows){
 	console.log(rows)
+/*
+[ { uid: '1', city: 'New York',     temp: [ 0, 35 ], country: 'USA' },
+  { uid: '3', city: 'Paris',        temp: [ 2, 33 ], country: 'France' },
+  { uid: '4', city: 'Marseille',    temp: [ 5, 27 ], country: 'France' },
+  { uid: '5', city: 'London',       temp: [ 2, 25 ], country: 'UK' } ]
+*/
 })
 
 // Get the one that does
-sequel.query.each('SELECT * FROM cities WHERE 15 = ANY (temp)', function(row){
+bk.query.each('SELECT * FROM cities WHERE 15 = ANY (temp)', function(row, query){
+	console.log(query)
 	console.log(row)
+/*
+	SELECT * FROM cities WHERE 15 = ANY (temp)
+{ uid: '2',  city: 'Los Angeles',  temp: [ 15, 35 ],  country: 'USA' }
+
+*/
+
 })
