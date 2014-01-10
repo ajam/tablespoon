f59ccb9d5deb9fa0b19ffd6086bf845d50bebb54
mhkeller <michael.keller@gmail.com>
2014-01-10 16:58:03 -0500

    more formatting

--- example/queries.js
+++ example/queries.js
@@ -33,8 +33,8 @@ bk.createTable('cities', data)
 // Get the rows that don't have 15
 bk.query('SELECT * FROM cities WHERE 15 != ALL (temp)', function(rows){
 	console.log(rows)
-/*
-[ { uid: '1', city: 'New York',     temp: [ 0, 35 ], country: 'USA' },
+/*[
+  { uid: '1', city: 'New York',     temp: [ 0, 35 ], country: 'USA' },
   { uid: '3', city: 'Paris',        temp: [ 2, 33 ], country: 'France' },
   { uid: '4', city: 'Marseille',    temp: [ 5, 27 ], country: 'France' },
   { uid: '5', city: 'London',       temp: [ 2, 25 ], country: 'UK' } ]*/
